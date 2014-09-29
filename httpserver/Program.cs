cf87fd8cdd252b6355eb922d827555ecb53b8db6
kaspertorian <kaspertorian@gmail.com>
2014-09-29 13:54:24 +0200

    har lavet serversocket.start();

--- httpserver/Program.cs
+++ httpserver/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,11 @@ namespace httpserver
     {
         static void Main(string[] args)
         {
+            TcpListener serversocket = new TcpListener(65080);
+            serversocket.Start();
+
+            TcpClient connectionSocket = serversocket.AcceptTcpClient();
+
             Console.WriteLine("Hello http server");
         }
     }
