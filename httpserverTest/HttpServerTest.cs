302f0dcfd529427cfab8d091896115d5606c7d02
kaspertorian <kaspertorian@gmail.com>
2014-10-02 13:09:54 +0200

    Ja....

--- httpserverTest/HttpServerTest.cs
+++ httpserverTest/HttpServerTest.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Net.Sockets;
 using System.Text;
-using httpserver;
+using HttpServer;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace httpserverTest
